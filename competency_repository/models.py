92b1de33eb55b6146c4afdcbbc6fcb8ff3f4bd09
David Trimoulet <davidt@hotmail.fr>
2019-07-17 16:48:39 +0200

    [UPDATE] model improvement to cope with projects, problems, workshop and template

--- competency_repository/models.py
+++ competency_repository/models.py
@@ -1,9 +1,26 @@
 from django.db import models
 
 
+class NewCenturySkill(models.Model):
+    new_century_skill = models.CharField(max_length=1024)
+
+
+class InvestigationMethod(models.Model):
+    investigation_method = models.CharField(max_length=1024)
+
+
+class InvolvingContext(models.Model):
+    involving_context = models.CharField(max_length=1024)
+
+
+class APPMetaData(models.Model):
+    new_century_skills = models.ManyToManyField(NewCenturySkill)
+    investigation_methods = models.ManyToManyField(InvestigationMethod)
+    involving_contexts = models.ManyToManyField(InvolvingContext)
+
+
 class ActionPlan(models.Model):
     action_plan_steps = models.CharField(max_length=1024)
-
 
 
 class BloomVerb(models.Model):
@@ -38,10 +55,14 @@ class SkillLevel(models.Model):
     skill_D = models.CharField(max_length=1024)
 
 
+class CctlAnswer(models.Model):
+    cctl_question_text = models.CharField(max_length=1024)
+
+
 class CctlQuestion(models.Model):
     cctl_skill = models.ManyToManyField(Skill)
     cctl_question_text = models.CharField(max_length=1024)
-    cctl_question_answers = models.CharField(max_length=1024)
+    cctl_question_answers = models.ManyToManyField(CctlAnswer)
 
 
 class CCTL(models.Model):
@@ -49,31 +70,98 @@ class CCTL(models.Model):
 
 
 class ExerciseManipulation(models.Model):
-    manipulation = models.CharField(max_length=1024)
+    manipulation_text = models.CharField(max_length=1024)
+    manipulation_image = models.CharField(max_length=256)
 
 
 class Exercise(models.Model):
+    exercise_skills = models.ManyToManyField(Skill)
     exercise_text = models.CharField(max_length=1024)
+    exercise_image = models.CharField(max_length=256)
     exercise_solution = models.CharField(max_length=1024)
     exercise_manipulations = models.ManyToManyField(ExerciseManipulation)
-    exercise_pub_date = models.DateTimeField('date published')
-
-
-class Project(models.Model):
-    project_text = models.CharField(max_length=1024)
+    exercise_pub_date = models.DateTimeField()
 
 
 class Role(models.Model):
     role = models.CharField(max_length=256)
 
 
+class Version(models.Model):
+    version_number = models.IntegerField()
+    version_text = models.IntegerField()
+    version_author = models.OneToOneField()
+    version_pub_date = models.DateTimeField()
+
+
 class ValidationQuestion(models.Model):
     validation_question_text = models.CharField(max_length=1024)
     validation_question_answer = models.CharField(max_length=1024)
+    validation_question_answer_schema = models.CharField(max_length=256)
 
 
-class Prosit(models.Model):
-    prosit_text = models.CharField(max_length=1024)
-    prosit_skill = models.ManyToManyField(Skill)
-    prosit_action_plan = models.ManyToManyField(ActionPlan)
-    prosit_validation_questions = models.ManyToManyField(ValidationQuestion)
+class Resource(models.Model):
+    resource_ref = models.CharField(max_length=1024)
+    resource_image = models.CharField(max_length=256)
+
+
+class KeyWord(models.Model):
+    keyword = models.CharField(max_length=1024)
+    definition = models.CharField(max_length=1024)
+    definition_schema = models.CharField(max_length=256)
+
+
+class Solution(models.Model):
+    solution_text = models.CharField(max_length=1024)
+    solution_image = models.CharField(max_length=256)
+
+
+class Hypothesis(models.Model):
+    Hypothesis_text = models.CharField(max_length=1024)
+    Hypothesis_answer = models.CharField(max_length=1024)
+    Hypothesis_answer_image = models.CharField(max_length=256)
+
+
+class HintAndAdvise(models.Model):
+    hint_and_advise_text = models.CharField(max_length=1024)
+
+
+class Deliverable(models.Model):
+    deliverable_text = models.CharField(max_length=1024)
+    project_skills = models.ManyToManyField(Skill)
+
+
+class Milestone(models.Model):
+    milestone_date = models.DateField()
+    milestone_deliverables = models.ManyToManyField(Deliverable)
+
+
+class Workshop(models.Model):
+    workshop_title = models.CharField(max_length=256)
+    workshop_text = models.CharField(max_length=1024)
+    workshop_steps = models.ManyToManyField(Exercise)
+    workshop_versions = models.ManyToManyField(Version)
+
+
+class Problem(models.Model):
+    problem_title = models.CharField(max_length=256)
+    problem_text = models.CharField(max_length=1024)
+    problem_keyword = models.ManyToManyField(KeyWord)
+    problem_problem = models.CharField(max_length=1024)
+    problem_skill = models.ManyToManyField(Skill)
+    problem_hint_and_advise = models.OneToOneField(HintAndAdvise)
+    problem_action_plan = models.ManyToManyField(ActionPlan)
+    problem_validation_questions = models.ManyToManyField(ValidationQuestion)
+    problem_resources = models.ManyToManyField(Resource)
+    problem_solution = models.ManyToManyField(Solution)
+    problem_versions = models.ManyToManyField(Version)
+
+
+class Project(models.Model):
+    project_text = models.CharField(max_length=1024)
+    project_problems = models.ManyToManyField(Problem)
+    project_resources = models.ManyToManyField(Resource)
+    project_milestones = models.ManyToManyField(Milestone)
+    project_versions = models.ManyToManyField(Version)
+
+
--- competency_repository/views.py
+++ competency_repository/views.py
@@ -6,3 +6,35 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Welcome to the competency repository")
+
+
+def get_skills(request):
+    return HttpResponse("all skills")
+
+
+def get_questions_for_skill(request, skill):
+    return HttpResponse("Question for a skill")
+
+
+def get_questions_for_a_prosit(request, prosit_number):
+    return HttpResponse("Question for a skill")
+
+
+def get_prosits(request):
+    return HttpResponse("Question for a skill")
+
+
+def generate_student_cctl(request):
+    return HttpResponse("Question for a skill")
+
+
+def generate_teacher_cctl(request):
+    return HttpResponse("Question for a skill")
+
+
+def add_question(request, question):
+    return HttpResponse("Question for a skill")
+
+
+def add_prosit(request, prosit):
+    return HttpResponse("Question for a skill")
